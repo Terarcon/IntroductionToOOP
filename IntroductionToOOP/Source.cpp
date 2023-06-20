1076b3d52ba618d012a0206bf152d41368450ec5
User <User@TERARCON>
2023-06-20 11:09:00 +0800

    Add project files.

---
+++ IntroductionToOOP/Source.cpp
@@ -0,0 +1,87 @@
+﻿#include<iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+
+	double distance(Point other) 
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+
+	void print()
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
+};
+
+double distance(Point A, Point B)
+{
+	//return sqrt(pow(A.get_x() - B.get_x(), 2) + pow(A.get_y() - B.get_y(), 2));
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
+
+
+
+//#define STRUCT_POINT
+//#define DISTANCE_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	int a;
+	Point A; //Объявление переменной A типа Point
+			 //Ф - объект структуры
+
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+#ifdef DISTANCE_CHECK
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << "A: \t" << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << "B: \t" << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки A до точки B: \t" << A.distance(B) << endl;
+	cout << "Расстояние от точки A до точки B: \t" << distance(A, B) << endl;
+	cout << "Расстояние от точки B до точки A: \t" << distance(B, A) << endl;
+#endif // DISTANCE_CHECK
+
+	Point A;
+	A.print();
+}
